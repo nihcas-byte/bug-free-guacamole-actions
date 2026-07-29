d95bae15747fc3910bb404e704262bb3b564aa36
nihcas-byte <tomersachin54@gmail.com>
2026-07-29 23:12:59 +0530

    added vars v1

--- main.tf
+++ main.tf
@@ -17,3 +17,8 @@ resource "azurerm_resource_group" "rg-2" {
   name     = "rg-tf-github-actions-2"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rg-3" {
+  name     = "rg-tf-github-actions-3"
+  location = "East US"
+}
